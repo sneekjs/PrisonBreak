8ea28a8cd87cf36dfcf794950d217e9f922f844a
Jorrit <Jorritslaats@gmail.com>
2019-01-30 16:02:17 +0100

    Add player controller

---
+++ Assets/Scripts/Actors/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿namespace PrisonBreak.Scripts.Actors
+{
+    using PrisonBreak.Scripts.Interfaces;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private float _interactionRange = 2f;
+
+        [SerializeField]
+        private Transform _playerHead;
+
+        private void Update()
+        {
+            if (Input.GetButtonDown("Action"))
+            {
+                Interact();
+            }
+            Debug.DrawRay(_playerHead.position, _playerHead.forward, Color.green);
+        }
+
+        private void Interact()
+        {
+            Ray r = new Ray(_playerHead.position, _playerHead.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(r, out hit, _interactionRange))
+            {
+                IInteractable i = hit.collider.gameObject.GetComponent<IInteractable>();
+                if (i != null)
+                {
+                    i.Action();
+                }
+            }
+        }
+    }
+}
